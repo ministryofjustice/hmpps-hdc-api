aef7122babb395a9ef4cf62b74edb94ff985d0b9
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-02-08 14:35:33 +0000

    CVSL-1616 fix linting (#15)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/subjectaccessrequests/SubjectAccessRequestService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/subjectaccessrequests/SubjectAccessRequestService.kt
@@ -7,25 +7,27 @@ import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.LicenceVersionRepositor
 
 @Service
 class SubjectAccessRequestService(
-    val licenceRepository: LicenceRepository,
-    val licenceVersionRepository: LicenceVersionRepository,
-    val auditEventRepository: AuditEventRepository,
+  val licenceRepository: LicenceRepository,
+  val licenceVersionRepository: LicenceVersionRepository,
+  val auditEventRepository: AuditEventRepository,
 ) {
-    fun getByPrisonNumber(prisonNumber: String): SarContent? {
-        val licences = licenceRepository.findAllByPrisonNumber(prisonNumber)
-        val licenceVersions = licenceVersionRepository.findAllByPrisonNumber(prisonNumber)
+  fun getByPrisonNumber(prisonNumber: String): SarContent? {
+    val licences = licenceRepository.findAllByPrisonNumber(prisonNumber)
+    val licenceVersions = licenceVersionRepository.findAllByPrisonNumber(prisonNumber)
 
-        val bookingIds = licences.map { it.bookingId } + licenceVersions.map { it.bookingId }
-        val events = auditEventRepository.findByBookingIds(bookingIds.map { it.toString() }.toSet())
+    val bookingIds = licences.map { it.bookingId } + licenceVersions.map { it.bookingId }
+    val events = auditEventRepository.findByBookingIds(bookingIds.map { it.toString() }.toSet())
 
-        return if (licences.isEmpty() && licenceVersions.isEmpty() && events.isEmpty())
-            null
-        else SarContent(
-            content = Content(
-                licences = licences,
-                licenceVersions = licenceVersions,
-                auditEvents = events,
-            ),
-        )
+    return if (licences.isEmpty() && licenceVersions.isEmpty() && events.isEmpty()) {
+      null
+    } else {
+      SarContent(
+        content = Content(
+          licences = licences,
+          licenceVersions = licenceVersions,
+          auditEvents = events,
+        ),
+      )
     }
+  }
 }
