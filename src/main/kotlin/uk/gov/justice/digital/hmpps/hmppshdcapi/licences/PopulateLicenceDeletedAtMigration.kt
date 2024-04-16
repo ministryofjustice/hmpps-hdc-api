5088dd13942b20d3ae579cd72e32c99fb92357cb
Louise Norris <louise.norris@digital.justice.gov.uk>
2024-04-16 17:37:20 +0100

    CVSL-1707 lint fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/PopulateLicenceDeletedAtMigration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/PopulateLicenceDeletedAtMigration.kt
@@ -1,14 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppshdcapi.licences
 
+import java.time.LocalDateTime
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Limit
 import org.springframework.data.domain.Page
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.prison.Booking
 import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.prison.PrisonApiClient
-import java.time.LocalDateTime
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 val UNKNOWN_DELETED_AT = null
 
