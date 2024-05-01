bf907c08e7223a37a0c715f31d5e0c7c04a3ee16
Louise Norris <louise.norris@digital.justice.gov.uk>
2024-05-01 15:40:56 +0100

    CVSL-1707 lint fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/LicenceVersionRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/LicenceVersionRepository.kt
@@ -1,7 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppshdcapi.licences
 
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
