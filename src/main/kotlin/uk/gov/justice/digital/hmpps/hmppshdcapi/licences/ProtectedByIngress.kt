5a03d7783a48086ecb8656c3889eca47b9535ff0
Louise N <louise.norris@digital.justice.gov.uk>
2024-06-28 12:47:51 +0100

    CVSL-1708 add protected by ingress annotation to job controller (#38)
    * CVSL-1708 add protected by ingress annotation to job controller
    
    * CVSL-1708 add end of line
    
    * CVSL-1708 rename ControllerAnnotations to fix lint

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/ProtectedByIngress.kt
@@ -0,0 +1,5 @@
+package uk.gov.justice.digital.hmpps.hmppshdcapi.licences
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+annotation class ProtectedByIngress
