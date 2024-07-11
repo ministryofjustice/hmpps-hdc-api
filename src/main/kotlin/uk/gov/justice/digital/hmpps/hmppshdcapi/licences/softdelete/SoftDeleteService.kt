63d0d557685011888db1134b8420dc1123bef8b0
Louise N <louise.norris@digital.justice.gov.uk>
2024-07-11 15:48:29 +0100

    CVSL-1708 add sleep to reset cron job (#46)
    * CVSL-1708 add sleep to reset cron job
    
    * CVSL-1708 move sleep in reset cron job

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/softdelete/SoftDeleteService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/softdelete/SoftDeleteService.kt
@@ -14,6 +14,7 @@ import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.LicenceVersionRepositor
 import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.prison.PrisonSearchApiClient
 import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.prison.Prisoner
 import uk.gov.justice.digital.hmpps.hmppshdcapi.util.AuditEventType
+import java.lang.Thread.sleep
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -59,6 +60,7 @@ class SoftDeleteService(
       batchSize = batchSize,
       totalBatches = totalBatches,
     )
+    sleep(2000)
   }
 
   @Transactional
