71d4e002751bc482445869a40556a662ccc8eb2f
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2024-08-13 11:39:19 +0100

    CVSL-2032 linting updates

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/prison/Prison.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/prison/Prison.kt
@@ -2,10 +2,10 @@ package uk.gov.justice.digital.hmpps.hmppshdcapi.licences.prison
 
 data class PrisonContactDetails(
   val agencyId: String,
-  val phones: Telephone
+  val phones: Telephone,
 )
 
 data class Telephone(
   val number: String,
-  val type: String
+  val type: String,
 )
