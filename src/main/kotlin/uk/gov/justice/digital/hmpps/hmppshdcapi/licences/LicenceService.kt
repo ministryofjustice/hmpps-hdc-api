ee2194c6faf720229bf874c12df3036260538c11
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2025-01-10 15:07:05 +0000

    CVSL-2329 run linter

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/LicenceService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/LicenceService.kt
@@ -30,7 +30,7 @@ class LicenceService(
     return HdcLicence(
       licenceId = licence.id,
       curfewAddress = getAddress(curfew, cas2Referral, proposedAddress),
-      firstNightCurfewHours = curfew?.firstNight?.let{ transformToModelFirstNight(it) },
+      firstNightCurfewHours = curfew?.firstNight?.let { transformToModelFirstNight(it) },
       // curfewHours referred to as curfewTimes in CVL as going forward a more suitable name and had to distinguish between the two different curfew data formats
       curfewTimes = transformToModelCurfewTimes(curfew?.curfewHours),
     )
