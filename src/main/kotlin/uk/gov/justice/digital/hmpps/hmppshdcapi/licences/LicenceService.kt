048b35e82e403edfc6f5f7a857387bce52b3963d
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2025-01-14 15:56:40 +0000

    Revert "Revert "CVSL-2329 add LocalTime for firstNight (#77)" (#85)"
    This reverts commit 7506948ae252d33a181e466db993828d9c307816.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/LicenceService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/LicenceService.kt
@@ -3,14 +3,10 @@ package uk.gov.justice.digital.hmpps.hmppshdcapi.licences
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppshdcapi.config.HmppsHdcApiExceptionHandler.NoDataFoundException
 import uk.gov.justice.digital.hmpps.hmppshdcapi.model.HdcLicence
-import java.time.DayOfWeek.FRIDAY
-import java.time.DayOfWeek.MONDAY
-import java.time.DayOfWeek.SATURDAY
-import java.time.DayOfWeek.SUNDAY
-import java.time.DayOfWeek.THURSDAY
-import java.time.DayOfWeek.TUESDAY
-import java.time.DayOfWeek.WEDNESDAY
-import java.time.LocalTime
+import uk.gov.justice.digital.hmpps.hmppshdcapi.model.transformToModelCurfewAddress
+import uk.gov.justice.digital.hmpps.hmppshdcapi.model.transformToModelCurfewTimes
+import uk.gov.justice.digital.hmpps.hmppshdcapi.model.transformToModelFirstNight
+import uk.gov.justice.digital.hmpps.hmppshdcapi.model.CurfewAddress as ModelCurfewAddress
 
 @Service
 class LicenceService(
@@ -31,18 +27,16 @@ class LicenceService(
     val curfew = licenceData.curfew
     val proposedAddress = licenceData.proposedAddress
 
-    return curfew?.curfewHours?.let { formatCurfewHoursObject(curfew.curfewHours) }?.let {
-      HdcLicence(
-        licenceId = licence.id,
-        curfewAddress = getAddress(curfew, cas2Referral, proposedAddress),
-        firstNightCurfewHours = curfew.firstNight,
-        // curfewHours referred to as curfewTimes in CVL as going forward a more suitable name and had to distinguish between the two different curfew data formats
-        curfewTimes = it,
-      )
-    }
+    return HdcLicence(
+      licenceId = licence.id,
+      curfewAddress = getAddress(curfew, cas2Referral, proposedAddress),
+      firstNightCurfewHours = curfew?.firstNight?.let { transformToModelFirstNight(it) },
+      // curfewHours referred to as curfewTimes in CVL as going forward a more suitable name and had to distinguish between the two different curfew data formats
+      curfewTimes = curfew?.let { transformToModelCurfewTimes(it.curfewHours) },
+    )
   }
 
-  fun getAddress(curfew: Curfew?, cas2Referral: Cas2Referral?, proposedAddress: ProposedAddress?): CurfewAddress? {
+  fun getAddress(curfew: Curfew?, cas2Referral: Cas2Referral?, proposedAddress: ProposedAddress?): ModelCurfewAddress? {
     val isCurfewApprovedPremisesRequired = curfew?.approvedPremises?.required == Decision.YES
     val isCas2ApprovedPremisesRequired = cas2Referral?.bassAreaCheck?.approvedPremisesRequiredYesNo == Decision.YES
     val isCas2Requested = cas2Referral?.bassRequest?.bassRequested == Decision.YES
@@ -54,62 +48,6 @@ class LicenceService(
       isCas2Requested && isCas2Accepted -> cas2Referral.bassOffer
       else -> proposedAddress?.curfewAddress
     }
-
-    return address?.let { formatAddress(it) }
-  }
-
-  private fun formatAddress(addressObject: Address): CurfewAddress =
-    CurfewAddress(
-      addressLine1 = addressObject.addressLine1,
-      addressLine2 = addressObject.addressLine2,
-      addressTown = addressObject.addressTown,
-      postCode = addressObject.postCode,
-    )
-
-  private fun formatCurfewHoursObject(curfewHours: CurfewHours): List<CurfewTimes> {
-    return listOf(
-      CurfewTimes(
-        fromDay = MONDAY,
-        fromTime = LocalTime.parse(curfewHours.mondayFrom),
-        untilDay = TUESDAY,
-        untilTime = LocalTime.parse(curfewHours.tuesdayUntil),
-      ),
-      CurfewTimes(
-        fromDay = TUESDAY,
-        fromTime = LocalTime.parse(curfewHours.tuesdayFrom),
-        untilDay = WEDNESDAY,
-        untilTime = LocalTime.parse(curfewHours.wednesdayUntil),
-      ),
-      CurfewTimes(
-        fromDay = WEDNESDAY,
-        fromTime = LocalTime.parse(curfewHours.wednesdayFrom),
-        untilDay = THURSDAY,
-        untilTime = LocalTime.parse(curfewHours.thursdayUntil),
-      ),
-      CurfewTimes(
-        fromDay = THURSDAY,
-        fromTime = LocalTime.parse(curfewHours.thursdayFrom),
-        untilDay = FRIDAY,
-        untilTime = LocalTime.parse(curfewHours.fridayUntil),
-      ),
-      CurfewTimes(
-        fromDay = FRIDAY,
-        fromTime = LocalTime.parse(curfewHours.fridayFrom),
-        untilDay = SATURDAY,
-        untilTime = LocalTime.parse(curfewHours.saturdayUntil),
-      ),
-      CurfewTimes(
-        fromDay = SATURDAY,
-        fromTime = LocalTime.parse(curfewHours.saturdayFrom),
-        untilDay = SUNDAY,
-        untilTime = LocalTime.parse(curfewHours.sundayUntil),
-      ),
-      CurfewTimes(
-        fromDay = SUNDAY,
-        fromTime = LocalTime.parse(curfewHours.sundayFrom),
-        untilDay = MONDAY,
-        untilTime = LocalTime.parse(curfewHours.mondayUntil),
-      ),
-    )
+    return address?.let { transformToModelCurfewAddress(it) }
   }
 }
