fe67f5d0b57196010a440c5a1e6b68f75c42dbb2
AledWEvans <aled.evans1@justice.gov.uk>
2026-02-16 09:52:53 +0000

    CVSL-3767 HDC Data Domains - Update to allow us to run a testing app

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/model/conditions/LicenceIdBatchRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/model/conditions/LicenceIdBatchRequest.kt
@@ -4,16 +4,16 @@ import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Size
 
-private const val maxBatchSize = 1000
+private const val MAX_BATCH_SIZE = 1000
 
 @Schema(description = "Request containing licence IDs to retrieve bespoke conditions for")
 data class LicenceIdBatchRequest(
 
-    @field:NotEmpty
-    @field:Size(max = maxBatchSize, message = "Maximum $maxBatchSize licence IDs allowed per request")
-    @field:Schema(
-        description = "List of licence IDs",
-        example = "[1, 2, 3]",
-    )
-    val licenceIds: List<Long>,
+  @field:NotEmpty
+  @field:Size(max = MAX_BATCH_SIZE, message = "Maximum $MAX_BATCH_SIZE licence IDs allowed per request")
+  @field:Schema(
+    description = "List of licence IDs",
+    example = "[1, 2, 3]",
+  )
+  val licenceIds: List<Long>,
 )
