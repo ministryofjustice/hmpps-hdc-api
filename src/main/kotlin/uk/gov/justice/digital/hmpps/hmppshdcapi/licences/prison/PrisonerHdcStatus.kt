c44df1f55d542a352d81be2efdc51c2e6ea5002d
AledWEvans <aled.evans1@justice.gov.uk>
2026-03-19 14:38:29 +0000

    CVSL-3921 Possible fix to a problem Jackson 4 not allowing default to false when boolen not given

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/prison/PrisonerHdcStatus.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/prison/PrisonerHdcStatus.kt
@@ -1,10 +1,13 @@
 package uk.gov.justice.digital.hmpps.hmppshdcapi.licences.prison
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class PrisonerHdcStatus(
   val approvalStatus: String? = null,
   val approvalStatusDate: String? = null,
   val bookingId: Long? = null,
   val checksPassedDate: String? = null,
+  @field:JsonProperty(defaultValue = "false")
   val passed: Boolean,
   val refusedReason: String? = null,
 ) {
