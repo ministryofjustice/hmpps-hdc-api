8424f2c17410315855dd6dd4080e11e747ed271a
AledWEvans <aled.evans1@justice.gov.uk>
2026-03-19 14:12:42 +0000

    CVSL-3921 Possible fix to a problem Jackson 4 not allowing default to false when boolen not given

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/config/JacksonConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/config/JacksonConfig.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppshdcapi.config
 import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import tools.jackson.databind.DeserializationFeature
 import tools.jackson.databind.ObjectMapper
 import tools.jackson.databind.json.JsonMapper
@@ -13,8 +14,16 @@ import tools.jackson.module.kotlin.KotlinModule
 class JacksonConfig {
 
   @Bean
-  fun objectMapper(): ObjectMapper = JsonMapper.builder()
-    .findAndAddModules() // auto-detect JavaTimeModule, ParameterNamesModule, etc.
+  @Primary
+  fun objectMapper(): ObjectMapper = customise(JsonMapper.builder()).build()
+
+  @Bean
+  fun customizer(): JsonMapperBuilderCustomizer = JsonMapperBuilderCustomizer { builder ->
+    builder?.let { customise(it) }
+  }
+
+  private fun customise(builder: JsonMapper.Builder): JsonMapper.Builder = builder
+    .findAndAddModules()
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     .disable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
     .addModule(
@@ -22,17 +31,4 @@ class JacksonConfig {
         .configure(KotlinFeature.StrictNullChecks, false)
         .build(),
     )
-    .build()
-
-  @Bean
-  fun customizer(): JsonMapperBuilderCustomizer = JsonMapperBuilderCustomizer { builder: JsonMapper.Builder? ->
-    builder!!
-      .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-      .disable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
-      .addModule(
-        KotlinModule.Builder()
-          .configure(KotlinFeature.StrictNullChecks, false)
-          .build(),
-      )
-  }
 }
