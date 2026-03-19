cc21ba1e3eb9c4ec5473348c487a6f6dc82318f2
AledWEvans <aled.evans1@justice.gov.uk>
2026-03-19 14:33:33 +0000

    CVSL-3921 Possible fix to a problem Jackson 4 not allowing default to false when boolen not given

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/config/JacksonConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/config/JacksonConfig.kt
@@ -3,29 +3,23 @@ package uk.gov.justice.digital.hmpps.hmppshdcapi.config
 import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import tools.jackson.databind.DeserializationFeature
-import tools.jackson.databind.json.JsonMapper
 import tools.jackson.module.kotlin.KotlinFeature
 import tools.jackson.module.kotlin.KotlinModule
 
 @Configuration
 class JacksonConfig {
 
-
-  @Primary
   @Bean
   fun customizer(): JsonMapperBuilderCustomizer = JsonMapperBuilderCustomizer { builder ->
-    builder?.let { customise(it) }
+    builder
+      .findAndAddModules()
+      .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+      .disable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
+      .addModule(
+        KotlinModule.Builder()
+          .configure(KotlinFeature.StrictNullChecks, false)
+          .build(),
+      )
   }
-
-  private fun customise(builder: JsonMapper.Builder): JsonMapper.Builder = builder
-    .findAndAddModules()
-    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-    .disable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
-    .addModule(
-      KotlinModule.Builder()
-        .configure(KotlinFeature.StrictNullChecks, false)
-        .build(),
-    )
 }
