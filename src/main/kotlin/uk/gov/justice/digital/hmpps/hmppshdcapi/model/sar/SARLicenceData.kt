8d6342e094fe447148bfccab0cc155f47cb1f377
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2026-03-26 16:39:57 +0000

    CVSL-3917 Fix HDC SAR template

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/model/sar/SARLicenceData.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/model/sar/SARLicenceData.kt
@@ -6,6 +6,8 @@ import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.Approval
 import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.BespokeCondition
 import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.Curfew
 import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.CurrentCas2Referral
+import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.Decision
+import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.DecisionMade
 import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.Document
 import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.Eligibility
 import uk.gov.justice.digital.hmpps.hmppshdcapi.licences.FinalChecks
@@ -25,6 +27,7 @@ import uk.gov.justice.digital.hmpps.hmppshdcapi.model.sar.SARConditionFormatter.
 import uk.gov.justice.digital.hmpps.hmppshdcapi.model.sar.SARConditionFormatter.getConditionValues
 import uk.gov.justice.digital.hmpps.hmppshdcapi.model.sar.SARConditionFormatter.getPolicyVersion
 import uk.gov.justice.digital.hmpps.hmppshdcapi.model.sar.SARConditionFormatter.policyVersions
+import uk.gov.justice.digital.hmpps.hmppshdcapi.util.StringListHolder
 import java.time.LocalDateTime
 import kotlin.Boolean
 
@@ -63,15 +66,48 @@ data class SARLicenceData(
   val proposedAddress: ProposedAddress?,
   val curfew: Curfew?,
   val risk: Risk?,
-  val reporting: Reporting?,
+  val reporting: SARReporting?,
   val victim: Victim?,
   val licenceConditions: SARConditions?,
   val document: Document?,
-  val approval: Approval?,
+  val approval: SARApproval?,
   val finalChecks: FinalChecks?,
   val variedFromLicenceNotInSystem: Boolean? = null,
   val vary: Vary? = null,
   val bassRejections: List<RejectedCas2Referral>? = null,
+)
+
+@JsonInclude(NON_NULL)
+data class SARApproval(
+  val release: SARRelease? = null,
+  val consideration: DecisionMade? = null,
+)
+
+@JsonInclude(NON_NULL)
+data class SARRelease(
+  val decision: Decision?,
+  val decisionMakerLastName: String?,
+  val reasonForDecision: String?,
+  val notedComments: String? = null,
+  // Can be a single value or an array
+  val reason: StringListHolder? = null,
+)
+
+@JsonInclude(NON_NULL)
+data class SARReporting(
+  val reportingInstructions: SARReportingInstructions,
+)
+
+@JsonInclude(NON_NULL)
+data class SARReportingInstructions(
+  val lastName: String?,
+  val postcode: String?,
+  val townOrCity: String?,
+  val organisation: String?,
+  val reportingDate: String?,
+  val reportingTime: String?,
+  val buildingAndStreet1: String?,
+  val buildingAndStreet2: String?,
 )
 
 @JsonInclude(NON_NULL)
@@ -86,9 +122,23 @@ data class SARConditions(
 data class SARAdditionalCondition(
   val text: String? = null,
   val fields: Map<String, Any>? = null,
-  val conditionCode: String,
   val renderedText: String? = null,
 )
+
+/**
+ * Extracts the surname from a full name in "firstname surname" format.
+ * Returns the surname if the name contains at least one space, otherwise returns null.
+ */
+fun extractLastname(fullName: String?): String? {
+  if (fullName.isNullOrBlank()) return fullName
+  val trimmed = fullName.trim()
+  val lastSpaceIndex = trimmed.lastIndexOf(' ')
+  return if (lastSpaceIndex > 0) {
+    trimmed.substring(lastSpaceIndex + 1)
+  } else {
+    null
+  }
+}
 
 fun Licence.toSAR() = SARLicence(
   licenceId = this.id!!,
@@ -122,7 +172,7 @@ fun LicenceData.toSAR(conditionVersion: Int? = null) = SARLicenceData(
   proposedAddress = this.proposedAddress,
   curfew = this.curfew,
   risk = this.risk,
-  reporting = this.reporting,
+  reporting = this.reporting.toSAR(),
   victim = this.victim,
   licenceConditions = this.licenceConditions?.let {
     SARConditions(
@@ -139,16 +189,57 @@ fun LicenceData.toSAR(conditionVersion: Int? = null) = SARLicenceData(
     )
   },
   document = this.document,
-  approval = this.approval,
+  approval = this.approval.toSAR(),
   finalChecks = this.finalChecks,
   variedFromLicenceNotInSystem = this.variedFromLicenceNotInSystem,
   vary = this.vary,
   bassRejections = this.bassRejections,
 )
 
+fun Reporting?.toSAR() = if (this == null) {
+  null
+} else {
+  SARReporting(
+    reportingInstructions =
+    with(this.reportingInstructions) {
+      SARReportingInstructions(
+        lastName = extractLastname(name),
+        postcode = postcode,
+        townOrCity = townOrCity,
+        organisation = organisation,
+        reportingDate = reportingDate,
+        reportingTime = reportingTime,
+        buildingAndStreet1 = buildingAndStreet1,
+        buildingAndStreet2 = buildingAndStreet2,
+      )
+    },
+  )
+}
+
+fun Approval?.toSAR(): SARApproval? = if (this == null) {
+  null
+} else {
+  SARApproval(
+    release = this.release?.let {
+      SARRelease(
+        decision = it.decision,
+        decisionMakerLastName = extractLastname(it.decisionMaker),
+        reasonForDecision = it.reasonForDecision,
+        notedComments = it.notedComments,
+        reason = it.reason,
+      )
+    },
+    consideration = this.consideration,
+  )
+}
+
 fun attemptToGuessVersion(additional: Map<String, Map<String, Any>>?): Int? = policyVersions.getPolicyVersion(additional?.map { condition -> condition.key } ?: emptyList())
 
-private fun toAdditionalCondition(conditionVersion: Int?, code: String, values: Map<String, Any>): SARAdditionalCondition {
+private fun toAdditionalCondition(
+  conditionVersion: Int?,
+  code: String,
+  values: Map<String, Any>,
+): SARAdditionalCondition {
   val conditionVersionData = LicenceConditionRenderer.getConditionTemplateVersion(conditionVersion)[code]!!
   val renderedTextData = LicenceConditionRenderer.renderCondition(conditionVersionData, values)
 
@@ -156,6 +247,5 @@ private fun toAdditionalCondition(conditionVersion: Int?, code: String, values: 
     text = policyVersions.getConditionText(conditionVersion, code),
     fields = policyVersions.getConditionValues(values),
     renderedText = renderedTextData,
-    conditionCode = code,
   )
 }
