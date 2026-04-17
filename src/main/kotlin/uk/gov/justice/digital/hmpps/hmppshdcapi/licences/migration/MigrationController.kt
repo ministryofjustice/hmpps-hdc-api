23df58bcc22e81cdd19959680c4acb5bcbf2101b
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2026-04-17 16:14:48 +0100

    CVSL-3836 Prog HDC - Migration - Migrate an Active case

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/migration/MigrationController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/migration/MigrationController.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -49,7 +50,7 @@ class MigrationController(
     return ResponseEntity.ok().build()
   }
 
-  @PostMapping("/{activeLicenceId}/to-cvl/preview")
+  @GetMapping("/{activeLicenceId}/to-cvl/preview")
   @PreAuthorize("hasAnyRole('HDC_ADMIN')")
   @Operation(
     summary = "Preview migration of a single active licence to CVL",
