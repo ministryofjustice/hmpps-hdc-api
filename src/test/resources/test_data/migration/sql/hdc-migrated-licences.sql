1dac4005162ab293dd40f98b41ff73ef31a3d294
Louise N <louise.norris@digital.justice.gov.uk>
2026-04-24 11:47:13 +0100

    CVSL-3836 add additional condition version (#186)
    * CVSL-3836 add additional condition version
    
    * CVSL-3836 lint fix

--- src/test/resources/test_data/migration/sql/hdc-migrated-licences.sql
+++ src/test/resources/test_data/migration/sql/hdc-migrated-licences.sql
@@ -243,7 +243,7 @@ VALUES (1,
             }
         }
                 }'::jsonb,
-        54222, 'ELIGIBILITY', 1, '2021-08-06 15:06:37.188', 0, '1', '2', 'A12345B', null, false),
+        54222, 'ELIGIBILITY', 1, '2021-08-06 15:06:37.188', 0, '2', '2', 'A12345B', null, false),
 
        -- Create the data for a HDC licence with a CAS2 address
        (2,
