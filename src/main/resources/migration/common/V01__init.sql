468734ff3221badd4f5ee62775b7cf85be0baf14
Louise Norris <louise.norris@digital.justice.gov.uk>
2026-05-06 14:47:00 +0100

    CVSL-4059 small edit to migration file

--- src/main/resources/migration/common/V01__init.sql
+++ src/main/resources/migration/common/V01__init.sql
@@ -1,9 +1,5 @@
 -- DROP SCHEMA public;
 
-CREATE SCHEMA public AUTHORIZATION pg_database_owner;
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
 -- DROP TYPE public."migration_error_source";
 
 CREATE TYPE public."migration_error_source" AS ENUM (
