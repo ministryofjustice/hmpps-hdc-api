f91931c3caed46043725c14b49d44df22ff0ed6c
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2026-06-25 15:58:03 +0100

    CVSL-4249 PROG HDC - VARY EA - Replace “Change” links with “Vary licence” row for licences with existing variations - changes to link with changes in licenses

--- src/main/resources/migration/common/V01__init.sql
+++ src/main/resources/migration/common/V01__init.sql
@@ -436,7 +436,8 @@ AS SELECT id,
           vary_version,
           prison_number,
           deleted_at,
-          licence_in_cvl
+          licence_in_cvl,
+          migration_state
    FROM licence_versions
    WHERE deleted_at IS NULL;
 
