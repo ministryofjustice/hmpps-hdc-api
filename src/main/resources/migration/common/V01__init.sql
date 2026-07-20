7f02394287cf37a2b79192ae42d1d6b85e1e6187
Louise Norris <louise.norris@digital.justice.gov.uk>
2026-07-20 14:41:15 +0100

    CVSL-4373 update licences db schema

--- src/main/resources/migration/common/V01__init.sql
+++ src/main/resources/migration/common/V01__init.sql
@@ -272,9 +272,9 @@ CREATE TABLE public.licence_migration_log (
                                               error_source public."migration_error_source" NULL,
                                               booking_id int4 NULL,
                                               prison_number varchar(7) NULL,
+                                              community_offender_manager_email varchar(255) NULL,
                                               CONSTRAINT licence_migration_log_pkey PRIMARY KEY (id)
 );
-
 CREATE INDEX idx_licence_migration_log_licence_id ON public.licence_migration_log USING btree (licence_version_id);
 
 -- Permissions
