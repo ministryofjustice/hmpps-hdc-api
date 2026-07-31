7935e5ec336d8bf95ce99f15c3a7d18adf471c62
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2026-07-31 14:03:43 +0100

    CVSL-4406 PROG HDC - Vary National Roll Out - Migrate active HDC licenses to CVL on a scheduled recurring basis

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/migration/MigrationJobController.kt
@@ -0,0 +1,42 @@
+package uk.gov.justice.digital.hmpps.hmppshdcapi.licences.migration
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppshdcapi.config.ROLE_HDC_ADMIN
+
+@Tag(name = "Jobs")
+@RestController
+class MigrationJobController(
+  private val migrationProcessService: MigrationProcessService
+) {
+
+  @PostMapping("/jobs/licences-migrate-batch-to-cvl")
+  @PreAuthorize("hasAnyRole('$ROLE_HDC_ADMIN')")
+  @Operation(
+    summary = "Migrate a batch of licences to CVL on a schedule",
+    description = "Triggers migration of licences into CVL on a schedule",
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "202",
+        description = "Migration schedule started successfully",
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Invalid request",
+      ),
+    ],
+  )
+  fun migrateABatchOfLicences(): ResponseEntity<String> {
+    migrationProcessService.migrateABatchOfLicences()
+    return ResponseEntity.accepted().body("Migration schedule started")
+  }
+
+}
