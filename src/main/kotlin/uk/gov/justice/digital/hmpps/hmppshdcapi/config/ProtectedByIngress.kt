f17d4bf08040c11050a6ab8a49a8c5ce2f62b36e
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2026-08-03 10:58:12 +0100

    CVSL-4406 PROG HDC - Vary National Roll Out - Migrate active HDC licenses to CVL on a scheduled recurring basis in non prod for 11:30am second attempt

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/config/ProtectedByIngress.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/config/ProtectedByIngress.kt
@@ -3,4 +3,3 @@ package uk.gov.justice.digital.hmpps.hmppshdcapi.config
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 annotation class ProtectedByIngress
-
