82dc702adcbca837823ce437904f163b8bd26b65
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2026-09-18 16:34:15 +0100

    Added log to help me test HDC event based migration, booking id are changing in nomis

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/migration/MigrationProcessService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/migration/MigrationProcessService.kt
@@ -165,7 +165,7 @@ class MigrationProcessService(
     throwEventProcessingExceptions: Boolean = false,
     migrationTrigger: MigrationTrigger,
   ) {
-    log.info("git checkout -: Processing licence version id {}", licenceDetail.licenceVersionId)
+    log.info("HDC migration: Processing licence version id {}", licenceDetail.licenceVersionId)
     try {
       migrationRequestService.validate(prisoner)
       migrationRequestService.migrateLicenceToCvl(licenceDetail, prisoner)
