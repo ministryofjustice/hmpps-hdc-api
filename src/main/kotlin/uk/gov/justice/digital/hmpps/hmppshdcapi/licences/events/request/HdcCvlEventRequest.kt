9afd3d5af2f8c2c38aa81fd6dd6f4fefb50fd5ab
nishanthmahasamudram <nishanth.mahasamudram@justice.gov.uk>
2026-09-21 16:56:52 +0100

    CVSL-4496-Prog-HDC-API-Receive-requests-from-HDC

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/events/request/HdcCvlEventRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppshdcapi/licences/events/request/HdcCvlEventRequest.kt
@@ -1,15 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppshdcapi.licences.events.request
 
-import jakarta.validation.constraints.NotBlank
 import uk.gov.justice.digital.hmpps.hmppshdcapi.util.HdcCvlEventType
 
 data class HdcCvlEventRequest(
   val eventType: HdcCvlEventType,
   val licenceId: Long,
   val bookingId: Long,
-  @field:NotBlank(message = "nomsNumber must be supplied")
-  val nomsNumber: String? = null,
-  @field:NotBlank(message = "triggeredBy must be supplied")
-  val triggeredBy: String? = null,
+  val nomsNumber: String,
+  val triggeredBy: String,
   val reason: String? = null,
 )
